9bff00fcce41ca1b0a300e0b732ec2394267c972
rofiq199 <55471142+rofiq199@users.noreply.github.com>
2020-05-05 23:47:03 +0700

    jadwal

--- application/controllers/admin/jadwal.php
+++ application/controllers/admin/jadwal.php
@@ -10,9 +10,57 @@ class jadwal extends CI_Controller {
     
 	public function index()
 	{     
+        $data['matkul']=$this->m_admin->get('matkul');
+        $data['waktu']=$this->m_admin->get('waktu');
+        $data['hari']=$this->m_admin->get('hari');
         $data['jadwal']= $this->m_admin->tampiljadwal();
         $this->load->view('admin/header'); 
         $this->load->view('admin/list_jadwal',$data);
         $this->load->view('admin/footer'); 
-	}
+  }
+
+  function add(){
+    $ruang = $this->input->post('ruang');
+    $waktu = $this->input->post('waktu');
+    $golongan = $this->input->post('golongan');
+    $hari = $this->input->post('hari');
+    $matkul = $this->input->post('matkul');
+  $data = array(
+    'kode_matkul' => $matkul,
+    'kode_waktu' => $waktu,
+    'kode_hari' => $hari,
+    'ruangan' => $ruang,
+    'golongan' => $golongan
+    );
+  $this->m_admin->add($data,'jadwal');
+  redirect(base_url('admin/jadwal'));
+  }
+
+  function update(){
+    $kode_jadwal = $this->input->post('kode_jadwal');
+    $ruang = $this->input->post('ruang');
+    $waktu = $this->input->post('waktu');
+    $golongan = $this->input->post('golongan');
+    $hari = $this->input->post('hari');
+    $matkul = $this->input->post('matkul');
+  
+    $data = array(
+      'kode_matkul' => $matkul,
+      'kode_waktu' => $waktu,
+      'kode_hari' => $hari,
+      'ruangan' => $ruang,
+      'golongan' => $golongan
+      );
+  
+    $where = array(
+      'kode_jadwal' => $kode_jadwal
+    );
+    $this->m_admin->update($where,$data,'jadwal');
+    redirect(base_url('admin/jadwal'));
+  }
+  function hapus($kode_jadwal){
+    $where = array('kode_jadwal' => $kode_jadwal);
+    $this->m_admin->hapus($where,'jadwal');
+    redirect(base_url('admin/jadwal'));
+  }
 }
--- application/views/admin/list_jadwal.php
+++ application/views/admin/list_jadwal.php
@@ -8,7 +8,7 @@
 							<div class="card">
 								<div class="card-header">
 									<div class="d-flex align-items-center">
-										<h4 class="card-title">Tambah Dosen</h4>
+										<h4 class="card-title">Tambah Jadwal</h4>
 										<button class="btn btn-primary btn-round ml-auto" data-toggle="modal" data-target="#tambah">
 											<i class="fa fa-plus"></i>
 											Tambah Jadwal
@@ -25,7 +25,7 @@
 														<span class="fw-mediumbold">
 														Tambah</span> 
 														<span class="fw-light">
-															Dosen
+															Jadwal
 														</span>
 													</h5>
 													<button type="button" class="close" data-dismiss="modal" aria-label="Close">
@@ -34,50 +34,71 @@
 												</div>
 												<div class="modal-body">
 													<p class="small">Isi data dengan benar</p>
-													<form>
+													<form action="<?= base_url('admin/jadwal/add'); ?>" method="POST">
 														<div class="row">
-															<div class="col-sm-12">
+															<div class="col-md-6 pr-0">
 																<div class="form-group form-group-default">
-																	<label>Nim</label>
-																	<input type="text" class="form-control" placeholder="Masukkan Nim Mahasiswa">
+																<label>Hari</label>
+																	<select name="hari" class="form-control" >
+																	<?php foreach ($hari as $a) {	
+																	?>
+																	<option value="<?=$a->kode_hari?>"><?= $a->nama_hari ?></option>
+																	<?php }?>
+																	</select>
 																</div>
 															</div>
-															<div class="col-sm-12">
+															<div class="col-md-6 ">
 																<div class="form-group form-group-default">
-																	<label>Name</label>
-																	<input id="addName" type="text" class="form-control" placeholder="fill name">
+																<label>Waktu</label>
+																	<select name="waktu" class="form-control" >
+																	<?php foreach ($waktu as $b) {	
+																	?>
+																	<option value="<?=$b->kode_waktu?>"><?= $b->waktu_mulai?>-<?= $b->waktu_selesai ?></option>
+																	<?php }?>
+																	</select>
 																</div>
 															</div>
 															<div class="col-md-6 pr-0">
-															<label>Matkul</label>
 																<div class="form-group form-group-default">
+																<label>Matkul</label>
+																	<select name="matkul" class="form-control" >
+																	<?php foreach ($matkul as $c) {	
+																	?>
+																	<option value="<?=$c->kode_matkul?>"><?= $c->nama_matkul ?></option>
+																	<?php }?>
+																	</select>
 																</div>
 															</div>
 															<div class="col-md-6">
 																<div class="form-group form-group-default">
-																	<label>Semester</label>
-																	<select name="semester" class="form-control" >
-																	<option value="1">1</option>
-																	<option value="2">2</option>
-																	<option value="3">3</option>
-																	<option value="4">4</option>
-																	<option value="5">5</option>
-																	<option value="6">6</option>
-																	<option value="7">7</option>
-																	<option value="8">8</option>
+																	<label>Golongan</label>
+																	<select name="golongan" class="form-control" >
+																	<option value="A">A</option>
+																	<option value="B">B</option>
+																	<option value="C">C</option>
+																	<option value="D">D</option>
+																	<option value="E">E</option>
 																	</select>
 																</div>
 															</div>
+															<div class="col-sm-12">
+																<div class="form-group form-group-default">
+																	<label>Ruangan</label>
+																	<input type="text" name="ruang" class="form-control" placeholder="Masukkan Ruangan">
+																</div>
+															</div>
 														</div>
-													</form>
+													
 												</div>
 												<div class="modal-footer no-bd">
-													<button type="button" id="addRowButton" class="btn btn-primary">Add</button>
+													<input type="submit" value="Tambah" class="btn btn-primary">
 													<button type="button" class="btn btn-danger" data-dismiss="modal">Close</button>
 												</div>
 											</div>
 										</div>
+										</form>
 									</div>
+									
 									<div class="table-responsive">
 										<table id="add-row" class="display table table-striped table-hover" >
 											<thead>
@@ -112,15 +133,99 @@
 													<td><?= $c->golongan?>/<?=$c->semester?></td>
 													<td>
 														<div class="form-button-action">
-															<button type="button" data-toggle="tooltip" title="" class="btn btn-link btn-primary btn-lg" data-original-title="Edit Task">
+															<button type="button" data-toggle="modal" data-target="#edit<?= $c->kode_jadwal?>" title="" class="btn btn-link btn-primary btn-lg" data-original-title="Edit Task">
 																<i class="fa fa-edit"></i>
 															</button>
-															<button type="button" data-toggle="tooltip" title="" class="btn btn-link btn-danger" data-original-title="Remove">
+															<button type="button"  onclick="window.location.href='<?= base_url('admin/jadwal/hapus/'.$c->kode_jadwal); ?>'"  data-toggle="tooltip" title="" class="btn btn-link btn-danger" data-original-title="Hapus">
 																<i class="fa fa-times"></i>
 															</button>
 														</div>
 													</td>
 												</tr>
+									<!-- Modal -->
+									<div class="modal fade" id="edit<?=$c->kode_jadwal?>" tabindex="-1" role="dialog" aria-hidden="true">
+										<div class="modal-dialog" role="document">
+											<div class="modal-content">
+												<div class="modal-header no-bd">
+													<h5 class="modal-title">
+														<span class="fw-mediumbold">
+														Edit</span> 
+														<span class="fw-light">
+															Jadwal
+														</span>
+													</h5>
+													<button type="button" class="close" data-dismiss="modal" aria-label="Close">
+														<span aria-hidden="true">&times;</span>
+													</button>
+												</div>
+												<div class="modal-body">
+													<p class="small">Isi data dengan benar</p>
+													<form action="<?= base_url('admin/jadwal/add'); ?>" method="POST">
+														<div class="row">
+															<div class="col-md-6 pr-0">
+																<div class="form-group form-group-default">
+																<label>Hari</label>
+																	<select name="hari" class="form-control" >
+																	<?php foreach ($hari as $a) {	
+																	?>
+																	<option value="<?=$a->kode_hari?>"><?= $a->nama_hari ?></option>
+																	<?php }?>
+																	</select>
+																</div>
+															</div>
+															<div class="col-md-6 ">
+																<div class="form-group form-group-default">
+																<label>Waktu</label>
+																	<select name="waktu" class="form-control" >
+																	<?php foreach ($waktu as $b) {	
+																	?>
+																	<option value="<?=$b->kode_waktu?>"><?= $b->waktu_mulai?>-<?= $b->waktu_selesai ?></option>
+																	<?php }?>
+																	</select>
+																</div>
+															</div>
+															<div class="col-md-6 pr-0">
+																<div class="form-group form-group-default">
+																<label>Matkul</label>
+																	<select name="matkul" class="form-control" >
+																	<?php foreach ($matkul as $c) {	
+																	?>
+																	<option value="<?=$c->kode_matkul?>"><?= $c->nama_matkul ?></option>
+																	<?php }?>
+																	</select>
+																</div>
+															</div>
+															<div class="col-md-6">
+																<div class="form-group form-group-default">
+																	<label>Golongan</label>
+																	<select name="golongan" class="form-control" >
+																	<option value="A">A</option>
+																	<option value="B">B</option>
+																	<option value="C">C</option>
+																	<option value="D">D</option>
+																	<option value="E">E</option>
+																	</select>
+																</div>
+															</div>
+															<div class="col-sm-12">
+																<div class="form-group form-group-default">
+																	<label>Ruangan</label>
+																	<input type="text" name="ruang" class="form-control" placeholder="Masukkan Ruangan">
+																</div>
+															</div>
+														</div>
+													
+												</div>
+												<div class="modal-footer no-bd">
+													<input type="submit" value="Tambah" class="btn btn-primary">
+													<button type="button" class="btn btn-danger" data-dismiss="modal">Close</button>
+												</div>
+											</div>
+										</div>
+										</form>
+									</div>
+									
+
 												<?php }?>
 											</tbody>
 										</table>
